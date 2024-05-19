8ca538ac4a722f046954cabaff5413f229f8208f
Pluz21 <vanleemput.tim@gmail.com>
2024-05-19 10:25:59 -0300

    Starting authentification tab refactoring

--- Assets/Scripts/Editor/AI_ImageGenerator_Editor.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using UnityEngine;
-
-public class AI_ImageGenerator_Editor : MonoBehaviour
-{
-    
-}
